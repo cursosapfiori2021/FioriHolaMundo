a6c0706b2cb34a39bbeefbe5520f8c2a4c076f7f
cursosapfiori2021 <cursosapfiori2021@gmail.com>
2021-08-19 22:19:54 +0000

    agregar archivo i18n

--- holamundo/webapp/test/integration/pages/View1.js
+++ holamundo/webapp/test/integration/pages/View1.js
@@ -15,7 +15,7 @@ sap.ui.define([
 						id: "app",
 						viewName: sViewName,
 						success: function () {
-							Opa5.assert.ok(true, "The " + sViewName + " view is displayed");
+							Opa5.assert.ok(true, "The " + sViewName + " view is +  displayed");
 						},
 						errorMessage: "Did not find the " + sViewName + " view"
 					});
